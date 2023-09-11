8717866c9ef0804a8b8792f528990e9c43215843
Sebastián Viana <vianasebastian2000@gmail.com>
2023-09-11 14:05:30 -0300

    Added label feature for hosts

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ import yaml
 from prometheus_client import start_http_server, Gauge
 
 # Initialize Prometheus metrics
-ping_latency = Gauge('ping_latency_ms', 'Ping latency in milliseconds', ['host'])
+ping_latency = Gauge('ping_latency_ms', 'Ping latency in milliseconds', ['host', 'label']) 
 
-def ping_host(host):
+def ping_host(host, label):
     try:
         # Run the ping command and capture the output
         ping_output = os.popen(f"ping -c 1 {host}").read()
@@ -19,10 +19,10 @@ def ping_host(host):
                 end = line.find(' ms', start)
                 if start != -1 and end != -1:
                     rtt = float(line[start:end])
-                    return rtt
-        return None  # Return None if no valid RTT is found in the output
+                    return rtt, label
+        return None, label  # Return None if no valid RTT is found in the output
     except Exception as e:
-        return None
+        return None, label
 
 def read_config(config_file):
     try:
@@ -35,18 +35,9 @@ def read_config(config_file):
 
 def main():
     config_file = "default.yml"
-    custom_config_file = "custom.yml"
 
     # Read the default configuration
     config = read_config(config_file)
-
-    # Check for a custom configuration file
-    if os.path.exists(custom_config_file):
-        custom_config = read_config(custom_config_file)
-        config.update(custom_config)
-
-    hosts = config['hosts']
-    ping_interval = config['ping_interval']
 
     # Read the HTTP server port from an environment variable or use 8080 as the default
     http_port = int(os.environ.get('HTTP_PORT', 8080))
@@ -55,13 +46,15 @@ def main():
     start_http_server(http_port)
 
     while True:
-        for host in hosts:
-            response_time = ping_host(host)
+        for host_info in config['hosts']:
+            host = host_info['ip']
+            label = host_info['label']
+            response_time, label = ping_host(host, label)
             if response_time is not None:
-                ping_latency.labels(host=host).set(response_time)  # Set the actual response time
+                ping_latency.labels(host=host, label=label).set(response_time)  # Set the actual response time
             else:
-                ping_latency.labels(host=host).set(0)  # Set 0 for failed ping
-        time.sleep(ping_interval)
+                ping_latency.labels(host=host, label=label).set(0)  # Set 0 for failed ping
+        time.sleep(config['ping_interval'])
 
 if __name__ == "__main__":
     main()
